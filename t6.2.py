84f6242e3fa4c50d9e1561af3557fa54616f34fb
gabriel-nadalin <gknadalin@gmail.com>
2023-08-20 19:50:42 -0300

    t6.2 funcionando

--- t6.2.py
+++ t6.2.py
@@ -4,13 +4,14 @@ T = 0.296
 x0 = 1
 t0 = 0
 n = 1
+prevx = 0
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
 def f(t, x):
-    return t*math.e**(x**2)
+    return t*math.exp(x**2)
 
 def phi2(t, x, h):
     csi1 = f(t, x)
@@ -30,20 +31,40 @@ def phi4(t, x, h):
     csi4 = f(t + h, x + h * csi3)
     return 1/6*csi1 + 2/6*csi2 + 2/6*csi3 + 1/6*csi4
 
-def rungeKutta(t, x, h, phiR):
-    while t <= T:
+def rungeKutta(t, x, n, phiR):
+    h = T/n
+    for _ in range(n):
         x = x + h * phiR(t, x, h)
         t += h
     return x
 
 while True:
     h = T/n
-    x = x0
-    t = t0
-    prevx = 0
-    nextx = rungeKutta(t0, x0, h, phi2)
+    nextx = rungeKutta(t0, x0, n, phi2)
     n += 1
     if truncate(prevx, 8) == truncate(nextx, 8):
+        print(truncate(prevx, 8), n)
         break
     prevx = nextx
-    print(nextx, n)
+    
+n = 1
+
+while True:
+    h = T/n
+    nextx = rungeKutta(t0, x0, n, phi3)
+    n += 1
+    if truncate(prevx, 8) == truncate(nextx, 8):
+        print(truncate(prevx, 8), n)
+        break
+    prevx = nextx
+    
+n = 1
+
+while True:
+    h = T/n
+    nextx = rungeKutta(t0, x0, n, phi4)
+    n += 1
+    if truncate(prevx, 8) == truncate(nextx, 8):
+        print(truncate(prevx, 8), n)
+        break
+    prevx = nextx
